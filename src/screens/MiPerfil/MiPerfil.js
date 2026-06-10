2382996eaf151e4ff52b53e301082e39d971ce6c
guadapaz1 <pazga@udesa.edu.ar>
2026-06-10 15:21:26 -0300

    detalles de estilo

--- src/screens/MiPerfil/MiPerfil.js
+++ src/screens/MiPerfil/MiPerfil.js
@@ -49,8 +49,8 @@ function MiPerfil(props) {
         <View style={styles.container}>
             <Text style={styles.title}>Mi Perfil</Text>
 
-            <Text style={styles.text}>Usuario: {username}</Text>
-            <Text style={styles.text}>Email: {auth.currentUser.email}</Text>
+            <Text style={styles.text}><text style={styles.negrita}>Usuario:</text> {username}</Text>
+            <Text style={styles.text}><text style={styles.negrita}>Email: </text>{auth.currentUser.email}</Text>
 
             <Pressable style={styles.button} onPress={() => logout()}>
                 <Text style={styles.buttonText}>Cerrar sesión</Text>
@@ -104,4 +104,7 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         marginBottom: 15,
     },
+    negrita: {
+        fontWeight: "bold"
+    }
 });
